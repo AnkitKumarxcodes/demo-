dc97505a1e0b15585d46f90bc3bf5b6c7e393952
Ankit Kumar <ankitsingh17082003@gmail.com>
2025-11-09 11:45:41 +0530

    Add src/components/layout/Footer.jsx

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-gray-200 py-4">
+      <div className="container mx-auto text-center">
+        <p>© 2023 ShopEase. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
